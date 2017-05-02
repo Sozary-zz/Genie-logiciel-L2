69d379ca273357265f936e07ecc65a79184d0147
mehdi <mehdi@DESKTOP-1BCOHJD>
2017-05-02 09:24:00 +0200

    Diagramme classes

--- GenieLog/Player.h
+++ GenieLog/Player.h
@@ -2,7 +2,7 @@
 #include <SFML\Graphics.hpp>
 #include "Entity.h"
 
-class Joueur : public Entite, public sf::Drawable, public sf::Transformable {
+class Joueur : public Entite/*, public sf::Drawable, public sf::Transformable */{
 
 
 public:
