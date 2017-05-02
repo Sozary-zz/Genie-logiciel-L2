10439786d94577bebe96c8d6ecd0511d69fed3a4
mehdi <mehdi@DESKTOP-1BCOHJD>
2017-05-02 10:42:27 +0200

    Player is running now!!

--- GenieLog/Player.cpp
+++ GenieLog/Player.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include "Player.h"
 
+using namespace sf;
 using namespace std;
 
-Joueur::Joueur() :nb_objet_max(30), nb_equipement_max(6), nb_competence_max(5)
+Joueur::Joueur() :nb_objet_max(30), nb_equipement_max(6), nb_competence_max(5),
+m_animated(seconds(.2f),true,false), m_is_walking(false)
 {
 	m_inventaire = new Objet[nb_objet_max];
 	m_equipement = new Objet[nb_equipement_max];
+
+	GameState::texture_manager->addElement("character_sprite", "data\\character.png");
+
+	m_walking_anim = new Animation[4]; // down/left/right/up
+
+	int y_pos = 0;
+	for (int i = 0; i < 4; ++i)
+	{
+		m_walking_anim[i].setSpriteSheet(GameState::texture_manager->getElement("character_sprite"));
+		switch (i)
+		{
+		case 0:y_pos = 640; break;
+		case 1:y_pos = 576; break;
+		case 2:y_pos = 704; break;
+		case 3:y_pos = 512; break;
+
+		}
+		for (int k = 0; k < 9; ++k)
+			m_walking_anim[i].addFrame(IntRect(64 * k, y_pos, 64, 64));
+	}
+	m_current_anim = &m_walking_anim[0];
 
 }
 
@@ -35,6 +58,35 @@ std::vector<std::string> Joueur::recupNomCompetences() const
 	return res;
 }
 
+void Joueur::run() {
+	m_is_walking = true;
+}
+
+void Joueur::up()
+{
+}
+
+void Joueur::down()
+{
+}
+
+void Joueur::left()
+{
+}
+
+void Joueur::right()
+{
+}
+
+bool Joueur::isWalking()
+{
+	return false;
+}
+
+void Joueur::stop_running()
+{
+}
+
 Joueur::Joueur(string pseudo, int pvmax, int mana, int armure, int force) : Entite(pseudo, pvmax, mana, armure, force), nb_objet_max(30), nb_equipement_max(6), nb_competence_max(5), m_pseudo(pseudo)
 {
 	m_inventaire = new Objet[nb_objet_max];
@@ -50,4 +102,8 @@ void Joueur::RempirCompetence()
 	tableau_competence.push_back(new Competence("Morsure", 25, 2, 10, 1));
 	tableau_competence.push_back(new Competence("Pet", 1, 0, 25, 0));
 	tableau_competence.push_back(new Competence("Mignon Sourire", 0, 0, 2, 0));
+}
+
+void Joueur::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
 }
--- GenieLog/Player.h
+++ GenieLog/Player.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <SFML\Graphics.hpp>
+#include "AnimatedSprite.hpp"
+#include "GameState.hpp"
 #include "Entity.h"
 
-class Joueur : public Entite/*, public sf::Drawable, public sf::Transformable */{
-
-
+class Joueur : public Entite, public sf::Drawable, public sf::Transformable {
+	
 public:
 
 	Joueur();
@@ -14,8 +15,16 @@ public:
 	Competence * choisir_competence(int i_comp);
 
 	std::vector<std::string> recupNomCompetences() const;
-
+	void run();
+	void up();
+	void down();
+	void left();
+	void right();
+	bool isWalking();
+	void stop_running();
 	void RempirCompetence();
+private:
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 private:
 	std::string m_pseudo;
@@ -25,6 +34,12 @@ private:
 	const int nb_competence_max;
 	const int nb_equipement_max;
 
+	Animation *m_walking_anim;
+	Animation* m_current_anim;
+	AnimatedSprite m_animated;
+
 	// SFML PART
-	//sf::Sprite 
+	sf::Sprite m_sprite;
+	
+	bool m_is_walking;
 };
